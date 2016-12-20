cd698517367d70385f416d9fa023acca991a5163
grumpyspaceman100 <grumpy_spaceman100@outlook.com>
2016-12-20 20:15:36 +0800

    Create main.cpp

---
+++ scripts/main.cpp
@@ -0,0 +1,8 @@
+function main () {
+wall → show back button(false)
+wall → set background(colors → black)
+data → board := media → create landscape board(1080, 1920)
+data → board → set background(colors → black)
+code → circleAnimation
+data → board → post to wall
+}
